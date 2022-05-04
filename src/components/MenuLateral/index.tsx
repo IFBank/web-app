3b96042dcbf26a291aa58de73f24598e0ea6e6ea
exodojaffar <exodojaffar@gmail.com>
2022-05-03 20:38:32 -0400

    add: init components folder and menulateral component

---
+++ src/components/MenuLateral/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import {
+	Container
+} from './styles';
+
+
+interface MenuLateralProps {
+};
+
+const MenuLateral: React.FC<MenuLateralProps> = () => {
+	return (
+		<Container>
+			
+		</Container>
+	);
+}
+
+export default MenuLateral;
