f36d584042510ff77be505ed00341e22080e1cd0
exodojaffar <exodojaffar@gmail.com>
2022-05-12 22:23:07 -0400

    create: pagina de listagem de encomendas
    pode ter algumas coisas ai

--- src/pages/EncomendasFlow/EncomendasPage/index.tsx
+++ src/pages/EncomendasFlow/EncomendasPage/index.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
+import BarraPesquisa from '../../../components/BarraPesquisa';
+import PedidoCard from '../../../components/PedidoCard';
+import MaterialIcon from '../../../components/MaterialIcon';
+
+
 import {
-	Container
+	Container,
+	PageIndicator,
+	Content,
+	ButtonPassPage,
+	PedidosGrid
 } from './styles';
 
 interface EncomendasPageProps {
@@ -10,7 +19,52 @@ interface EncomendasPageProps {
 const EncomendasPage: React.FC<EncomendasPageProps> = () => {
 
 	return (
-		<Container>
+		<Container 
+			titleHeader="Encomendas" 
+			subTitleHeader="Encontre aqui os pedidos requisitados pelo nosso aplicativo"
+		>
+			<BarraPesquisa>
+				<PageIndicator>
+					1/2
+				</PageIndicator>
+			</BarraPesquisa>
+			
+			<Content>
+				<ButtonPassPage>
+					<MaterialIcon name="arrow_back" size={26} color="black" />	
+				</ButtonPassPage>
+				
+
+				<PedidosGrid>
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+
+					
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+
+					
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+					<PedidoCard	gradient="secondary"/>
+					<PedidoCard	gradient="primary"/>
+
+				</PedidosGrid>
+
+				<ButtonPassPage>
+					<MaterialIcon name="arrow_forward" size={26} color="black" />	
+				</ButtonPassPage>
+			</Content>
+
 		</Container>
 	);
 }
--- src/pages/EncomendasFlow/EncomendasPage/styles.ts
+++ src/pages/EncomendasFlow/EncomendasPage/styles.ts
@@ -4,4 +4,40 @@ import BasePage from '../../../components/BasePage'
 
 // COMPONENTES DE BASE
 export const Container = styled(BasePage)`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const PageIndicator = styled.span`
+	font-family: Mukta;
+	font-size: 30px;
+	font-weight: 400;
+	text-align: center;
+`;
+
+export const Content = styled.div`
+	display: flex;
+	flex-direction: row;
+	flex: 0.5;
+`;
+
+export const ButtonPassPage = styled.div`
+
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	cursor: pointer;
+`;
+
+export const PedidosGrid = styled.div`
+	display: grid;
+	grid-template-columns: repeat(4, 200px);
+	grid-template-rows: repeat(4, 120px);
+	gap: 16px;
+	justify-content: center;
+
+	height: min-content;
+	flex: 1;
+	padding: 0 12px;
 `;
