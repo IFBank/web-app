a94f12e64e0e1933e115b2899c108ca53922a1f1
exodojaffar <exodojaffar@gmail.com>
2022-05-12 22:21:21 -0400

    create: botão generico

--- src/components/GenericButton/index.tsx
+++ src/components/GenericButton/index.tsx
@@ -1,16 +1,35 @@
 import React from 'react';
 
 import {
-	Container
+	Container,
+	MaterialIconStyled
 } from './styles';
 
 interface GenericButtonProps {
+	gradient?: string;
+	borderRadius?: number;
+	text?: string;
+	fontColor?: string;
+	iconName?: string;
+	iconSize?: number;
 };
 
-const GenericButton: React.FC<GenericButtonProps> = ({ ... rest}) => {
+const GenericButton: React.FC<GenericButtonProps> = (
+	{ 
+		gradient="primary", 
+		borderRadius=10,
+		text="TESTE", 
+		fontColor="white", 
+		iconName, 
+		iconSize=24, 
+		... rest 
+	}
+) => {
 
 	return (
-		<Container { ... rest }>
+		<Container fontColor={fontColor} backgroundColor={`--linear-${gradient}`} borderRadius={borderRadius}  { ... rest }>
+			{ iconName && <MaterialIconStyled name={iconName} size={iconSize} color={fontColor}/> }
+			<span>{text}</span>
 		</Container>
 	);
 }
--- src/components/GenericButton/styles.ts
+++ src/components/GenericButton/styles.ts
@@ -1,5 +1,35 @@
 import styled from 'styled-components';
 
+import MaterialIcon from '../MaterialIcon';
+
+interface ContainerProps {
+	fontColor: string;
+	backgroundColor: string;
+	borderRadius: number;
+};
+
 // COMPONENTES DE BASE
-export const Container = styled.div`
+export const Container = styled.div<ContainerProps>`
+	background: var(${ ({backgroundColor}) => backgroundColor});
+	border-radius: ${ ({borderRadius}) =>  `${borderRadius}px` };
+	padding: 10px 32px;
+
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	box-shadow: 0px 4px 4px 0px #00000040;
+
+	cursor: pointer;
+
+	> span{
+		color: ${({fontColor}) => fontColor};
+		font-family: Ubuntu;
+		font-size: 25px;
+		font-weight: 700;
+	}
+`;
+
+export const MaterialIconStyled = styled(MaterialIcon)`
+	margin-right: 12px;
 `;
