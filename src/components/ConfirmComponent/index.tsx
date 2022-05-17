a51f18ab351ee1e75f444665575420383b6a1342
exodojaffar <exodojaffar@gmail.com>
2022-05-17 18:03:31 -0400

    create: confirm component

--- src/components/ConfirmComponent/index.tsx
+++ src/components/ConfirmComponent/index.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 
 import {
-	Container
+	Container,
+	IconButton
 } from './styles'
 
 interface ComponentProps {
+	onCancel?: () => void;
+	onConfirm?: () => void;
 };
 
 const Component: React.FC<ComponentProps> = ({ ... rest }) => {
 	
 	return (
 		<Container { ... rest } > 
+			<span>Confirmar? </span>
+			<IconButton bgColor="semantic-red" name="clear" size={40} color="white"/>
+			<IconButton bgColor="primary" name="check" size={40} color="white"/>
 		</Container> 
 	);
 
--- src/components/ConfirmComponent/styles.ts
+++ src/components/ConfirmComponent/styles.ts
@@ -1,3 +1,41 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+import React from 'react';
+
+import MaterialIcon from '../MaterialIcon'
+
+interface IIconButtonProps {
+	bgColor: string;
+	name: string;
+	color: string;
+	size: number;
+};
+
+export const Container = styled.div`
+	display: flex;
+	align-items: center;
+
+	> span{
+		font-family: Ubuntu;
+		font-weight: 700;
+		font-size: 24px;
+		color: black;
+	}
+
+`;
+
+export const IconButton = styled(MaterialIcon)`
+	border-radius: 5px;
+	background: var(${ ({bgColor}) => `--linear-${bgColor}`});
+
+	display: flex:
+	justify-content: center;
+	align-items: center;
+
+	cursor: pointer;
+
+	margin-left: 24px;
+
+	width: 40px;
+	height: 40px;
+`
