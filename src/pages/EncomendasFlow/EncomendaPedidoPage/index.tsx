928d5a38f84f82c81de640d6cad39ceac9135b44
exodojaffar <exodojaffar@gmail.com>
2022-05-17 20:12:16 -0400

    create: page pedido encomendas

--- src/pages/EncomendasFlow/EncomendaPedidoPage/index.tsx
+++ src/pages/EncomendasFlow/EncomendaPedidoPage/index.tsx
@@ -1,19 +1,63 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import BackButton from '../../../components/BackButton'
+
+import ItemInfoEncomendas from '../../../components/ItemInfoEncomendas'
+import ConfirmComponent from '../../../components/ConfirmComponent'
 
 import {
-	Container
+	Container,
+	PedidoInfoContainer,
+	PedidoTitle,
+	InfoPedidoContainer,
+	ItemsContainer,
+	GenericButtonEncomendas,
+	ConfirmConteiner,
+
 } from './styles';
 
 interface EncomendaPedidoPageProps {
 };
 
 const EncomendaPedidoPage: React.FC<EncomendaPedidoPageProps> = () => {
+	const [confirmPedido, setConfirmPedido] = useState(false);
 
 	return (
 		<Container
 			titleHeader="Encomendas" 
 			subTitleHeader="Encontre aqui os pedidos requisitados pelo nosso aplicativo"
 		>
+			<BackButton />
+
+			<PedidoInfoContainer>
+				<PedidoTitle>
+					Pedido ID
+				</PedidoTitle>
+				<InfoPedidoContainer>
+					<p> Feito por: <span></span></p>
+					<p> Data: <span></span></p>
+					<p> Total<span></span></p>
+				</InfoPedidoContainer>
+			</PedidoInfoContainer>
+
+			<ItemsContainer>
+				<ItemInfoEncomendas />
+				<ItemInfoEncomendas />
+			</ItemsContainer>
+
+			<ConfirmConteiner>
+			
+			{
+			!confirmPedido ? (
+				<> 	
+					<GenericButton text="Cancelar" gradient="semantic-red" iconName="highlight_off" />
+					<GenericButton text="Confirmar" iconName="check_circle_outline" />
+				</> ) : <ConfirmComponent />
+			}
+			
+			</ConfirmConteiner>
+		
+
 		</Container>
 	);
 }
--- src/pages/EncomendasFlow/EncomendaPedidoPage/styles.ts
+++ src/pages/EncomendasFlow/EncomendaPedidoPage/styles.ts
@@ -1,7 +1,51 @@
 import styled from 'styled-components';
 
+import GenericButton from '../../../components/GenericButton'
 import BasePage from '../../../components/BasePage'
 
 // COMPONENTES DE BASE
 export const Container = styled(BasePage)`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const PedidoInfoContainer = styled.div`
+	background: var(--linear-secondary);
+	color: white;
+
+	border-radius: 5px;
+
+	justify-content: center;
+
+	padding: 12px;
+
+	margin-bottom: 30px;
+`;
+
+export const PedidoTitle = styled.p`
+	text-align: center;
+	margin-bottom: 24px;
+`;
+
+export const InfoPedidoContainer = styled.div`
+	display: flex;
+	flex: 1;
+	justify-content: space-between;
+`;
+
+export const ItemsContainer = styled.div`
+	display: flex;
+	flex: 1;
+	flex-wrap: wrap;
+	padding: 16px 32px;
+`;
+
+export const GenericButtonEncomendas = styled(GenericButton)`
+	padding-left: 49px;
+	padding-right: 49px;
+`;
+
+export const ConfirmConteiner = styled.div`
+	display: flex;
+	justify-content: space-evenly;
 `;
