e3c542574168105b308e20976d75588466f617c1
exodojaffar <exodojaffar@gmail.com>
2022-05-19 22:36:52 -0400

    confirm page

--- src/pages/PedidosFlow/PedidoConfirmPage/index.tsx
+++ src/pages/PedidosFlow/PedidoConfirmPage/index.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import BackButton from '../../../components/BackButton'
+
+import ConfirmComponent from '../../../components/ConfirmComponent'
+import GenericButton from '../../../components/GenericButton'
+
+import ItemQuantCard from '../../../components/ItemQuantCard';
+
 
 import {
-	Container
+	Container,
+	PedidoInfoContainer,
+	ItemsContainer,
+
+	CarrinhoText,
+	TotalText,
 } from './styles';
 
 interface PedidoConfirmPageProps {
 };
 
 const PedidoConfirmPage: React.FC<PedidoConfirmPageProps> = () => {
+	const [confirm, setConfirm] = useState(false);
 
 	return (
 		<Container
 			titleHeader="Pedidos" 
 			subTitleHeader="Crie aqui os pedidos requisitados pessoalmente"
+			textCancelButton="Cancelar pedido"
 		>
+			<BackButton />
+			
+			<PedidoInfoContainer>
+				
+				<CarrinhoText>
+					Carrinho <span>#Pedido {"ID"}</span>
+				</CarrinhoText>	
+
+				<TotalText>
+					Total: <span>R$ {"19,90"}</span>
+				</TotalText>
+
+				{
+					!confirm ? <GenericButton text="Finalizar pedido" iconName="check_circle_outline" iconSize={30}/> : <ConfirmComponent />
+				}
+			</PedidoInfoContainer>
+			<ItemsContainer>
+				<ItemQuantCard />	
+			</ItemsContainer>
+			
+
 		</Container>
 	);
 }
--- src/pages/PedidosFlow/PedidoConfirmPage/styles.ts
+++ src/pages/PedidosFlow/PedidoConfirmPage/styles.ts
@@ -4,4 +4,44 @@ import BasePage from '../../../components/BasePage'
 
 // COMPONENTES DE BASE
 export const Container = styled(BasePage)`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const PedidoInfoContainer = styled.div`
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+
+	margin-bottom: 12px;
+`;
+
+export const CarrinhoText = styled.p`
+	
+	font-family: Ubuntu;
+	font-size: 24px;
+	font-weight: 700;
+	color: black;
+
+	> span{
+		font-family: Mukta;
+		font-size: 16px;
+		font-weight: 400;
+	}
+`;
+
+export const TotalText = styled.p`
+	font-family: Ubuntu;
+	font-size: 18px;
+	font-weight: 700;
+
+	color: black;
+
+	> span{
+		color: #32DC32;
+	}
+`;
+
+export const ItemsContainer = styled.div`
+	flex: 1;
 `;
