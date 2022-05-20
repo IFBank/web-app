2891d1df58721a64126b274214110d692cf534b7
exodojaffar <exodojaffar@gmail.com>
2022-05-19 21:19:43 -0400

    create: item quant component

--- src/components/ItemQuantCard/index.tsx
+++ src/components/ItemQuantCard/index.tsx
@@ -1,49 +1,48 @@
 import React, { useState } from "react";
 
-import MaterialIcon from "../../components/MaterialIcon";
-
 import {
   Container,
   InfoContainer,
   ActionContainer,
   QuantContainer,
-  ActionButton,
+  ImageInfoContainer,
+  ActionButtonIcon,
+  ActionButtonText,
   QuantText,
 } from "./styles";
 
-interface PedidoQuantCardProps {}
+interface PedidoQuantCardProps {};
 
 const PedidoQuantCard: React.FC<PedidoQuantCardProps> = () => {
   const [quantValue, setQuantValue] = useState<number>(0);
 
+  // TODO: consertar estilização dos textos.
+
   return (
     <Container>
-      <img src="" />
+      <ImageInfoContainer>
+        <img src="" />
 
-      <InfoContainer>
-        <p>Nome</p>
-        <p>Valor</p>
-        <p>Estoque</p>
-      </InfoContainer>
+        <InfoContainer>
+          <p>Nome</p>
+          <p>Valor</p>
+          <p>Estoque</p>
+        </InfoContainer>
+      </ImageInfoContainer>
+      
 
-      {/* <ActionContainer>
-				<QuantContainer>
-					<ActionButton backgroundColor="--linear-semantic-red">
-						<MaterialIcon name="task" size="30" color="white" />
-					</ActionButton>
+      <ActionContainer>
+        <QuantContainer>
+          <ActionButtonIcon bgColor="semantic-red" name="task" size={30} color="white" />
+          <QuantText>{quantValue}</QuantText>
+          <ActionButtonIcon bgColor="primary" name="add" size={30} color="white" />
+        </QuantContainer>
 
-					<QuantText>{quantValue}</QuantText>
+        <ActionButtonText bgColor="primary">
+          Adicionar
+        </ActionButtonText>
 
-					<ActionButton backgroundColor="--linear-primary">
-						<MaterialIcon name="task" size="30" color="white" />
-					</ActionButton>
-				</QuantContainer>
-
-				<ActionButton backgroundColor="--linear-primary">
-					Adicionar
-				</ActionButton>
-
-			</ActionContainer> */}
+      </ActionContainer>
     </Container>
   );
 };
--- src/components/ItemQuantCard/styles.ts
+++ src/components/ItemQuantCard/styles.ts
@@ -1,49 +1,94 @@
 import styled from "styled-components";
 
-interface ActionButtonProps {
-  backgroundColor: string;
-}
+import MaterialIcon from "../../components/MaterialIcon";
+
+interface IActionButtonTextProps{
+  bgColor: string;
+};
 
 // COMPONENTES DE BASE
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  flex: 1;
 
-  padding: 6px 12px;
+  padding: 8px 16px;
 
-  border: var(--linear-primary) solid 1px;
+  border: #32DC32 solid 2x;
+
+  border-radius: 8px;
 
   img {
     width: 140px;
     height: 100px;
     background-color: black;
+    border-radius: 10px;
   }
+`;
+
+export const ImageInfoContainer = styled.div`
+  display: flex;
 `;
 
 export const InfoContainer = styled.div`
   margin-left: 4px;
+  display: flex;
+  justify-content: center;
+
+  flex-direction: column;
+
 `;
 
 export const ActionContainer = styled.div`
   display: flex;
   flex-direction: column;
+
+  width: 100px;
 `;
 
 export const QuantContainer = styled.div`
   display: flex;
+
+  justify-content: space-between;
+  flex: 1;
+
+  margin-bottom: 8px;
 `;
 
-export const ActionButton = styled.div<ActionButtonProps>`
-  color: white;
-
+export const ActionButtonIcon = styled(MaterialIcon)`
+  background: ${ ({bgColor})=> `var( --linear-${bgColor} )`};
   cursor: pointer;
 
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  padding: 8px 10px;
+
+  border-radius: 5px; 
+`;
+
+export const ActionButtonText = styled.div<IActionButtonTextProps>`
+  color: white;
+  background: ${ ({bgColor})=> `var( --linear-${bgColor} )`};
+  cursor: pointer;
+  border-radius: 5px; 
   padding: 4px 8px;
+
+  text-align: center;
 `;
 
 export const QuantText = styled.span`
-  background-color: gray;
-
+  background-color: #CCC;
   border-bottom: black solid 2px;
+  width: 16px;
+
+  font-size: 20px;
+
+  padding: 0px 10px;
+
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
 `;
