95f3c5496ea116595e0b68a25a1b16a686883a31
exodojaffar <exodojaffar@gmail.com>
2022-05-20 22:16:13 -0400

    faz algo

--- src/components/DashedBoxPedidos/styles.ts
+++ src/components/DashedBoxPedidos/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	border: 1px dashed #616161;
+
+	margin: 20px auto;
+
+	width: 60%;
+`;
