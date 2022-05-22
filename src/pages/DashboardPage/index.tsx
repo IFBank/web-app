feef3a4e082072a5c581264db984e487ddb6834b
exodojaffar <exodojaffar@gmail.com>
2022-05-21 21:10:39 -0400

    create: dashboard

--- src/pages/DashboardPage/index.tsx
+++ src/pages/DashboardPage/index.tsx
@@ -1,19 +1,61 @@
 import React from 'react';
 
+import BarraPesquisa from '../../components/BarraPesquisa'
+import FilterSearch from '../../components/FilterSearch'
+
+import TopTabBox from '../../components/TopTabBox'
+import PedidoCardDashBoard from '../../components/PedidoCardDashBoard'
+
 import {
-	Container
+	Container,
+
+	HistoricoContainer
 } from './styles';
 
 interface DashboardPageProps {
 };
 
+interface IDataTopTabBox {
+	name: string;
+	icon: string;
+	text: string;
+	Component: React.FC;
+}
+
+const Historico: React.FC = () => {
+	return(
+		<HistoricoContainer>
+
+			<BarraPesquisa>
+				<FilterSearch />
+			</BarraPesquisa>
+
+			<PedidoCardDashBoard />
+			<PedidoCardDashBoard />
+			<PedidoCardDashBoard />
+
+		</HistoricoContainer>
+	);
+};
+
 const DashboardPage: React.FC<DashboardPageProps> = () => {
+
+	const data: IDataTopTabBox[] = [{
+		name: "historico",
+		icon:"history", 
+		text:"Historico",
+		Component: Historico
+	}];
 
 	return (
 		<Container
 			titleHeader="Dashboard" 
 			subTitleHeader="Encontre aqui informações úteis referentes aos pedidos e a movimentação de dinheiro"
 		>
+			<TopTabBox data={data} />
+				
+			
+			
 		</Container>
 	);
 }
--- src/pages/DashboardPage/styles.ts
+++ src/pages/DashboardPage/styles.ts
@@ -4,4 +4,15 @@ import BasePage from '../../components/BasePage'
 
 // COMPONENTES DE BASE
 export const Container = styled(BasePage)`
+	display: flex;
+	flex-direction: column;
+`;
+
+export const HistoricoContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+
+	flex: 1;
+
+	padding: 0 16px;
 `;
