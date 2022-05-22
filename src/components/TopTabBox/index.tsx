be2442fbdb92f27e4ce0a3115de6b249493290a7
exodojaffar <exodojaffar@gmail.com>
2022-05-21 20:54:10 -0400

    init: top tab nav com comportamento basico com gambiarra

--- src/components/TopTabBox/index.tsx
+++ src/components/TopTabBox/index.tsx
@@ -1,20 +1,56 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+import MaterialIcon from '../MaterialIcon'
 
 import {
-	Container
+	Container,
+	HeaderContainer,
+	HeaderTab
 } from './styles'
 
-interface ComponentProps {
+interface IDataTopTabBox {
+	icon: string;
+	name: string;
+	text: string;
+	Component: React.FC;
+}
+
+interface TopTabBoxProps {
+	data?: IDataTopTabBox[];
 };
 
-const Component: React.FC<ComponentProps> = ({ ... rest }) => {
+const testData: IDataTopTabBox[] = [{
+		name: "historico",
+		icon:"history", 
+		text:"Historico",
+		Component: () => (<p>Teste</p>)
+}]
+
+const TopTabBox: React.FC<TopTabBoxProps> = ({ data=testData, ... rest }) => {
 	
+	const [actualPageIndex, setActualPageIndex] = useState<number>(0);
+
+	const pageInfo = data[actualPageIndex];
+
 	return (
 		<Container { ... rest } > 
+			<HeaderContainer>
+				{
+					data.map( (value, index) => (
+						<HeaderTab key={index} selected={index == actualPageIndex}>
+							<MaterialIcon name={value.icon} size={26} color="black"/>
+							<span>{value.text}</span>
+						</HeaderTab>
+					))
+				}
+			</HeaderContainer>
+
+			<pageInfo.Component />
+
 		</Container> 
 	);
 
 };
 
-export default Component;
+export default TopTabBox;
 
--- src/components/TopTabBox/styles.ts
+++ src/components/TopTabBox/styles.ts
@@ -1,3 +1,35 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+interface IHeaderTabProps {
+	selected: boolean;
+	key: number;
+}
+
+export const Container = styled.div`
+	border-radius: 10px;
+	box-shadow: 0px 4px 4px 0px #00000040;
+`;
+
+export const HeaderContainer = styled.div`
+	display: flex;
+	box-shadow: 0px 4px 4px 0px #00000040;
+
+	margin-bottom: 24px;
+
+`;
+
+export const HeaderTab = styled.div<IHeaderTabProps>`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	padding: 12px 0;
+
+	flex: 1;
+	
+	${ ({selected}) => selected && "border-bottom: #32DC32 solid 3px;"}
+
+	> span {
+		margin-left: 12px;
+	}
+`;
