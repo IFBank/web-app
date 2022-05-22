a56513dffcf212ba1b28ff3b321777c064978206
exodojaffar <exodojaffar@gmail.com>
2022-05-21 21:25:40 -0400

    init: filter component

--- src/components/FilterSearch/index.tsx
+++ src/components/FilterSearch/index.tsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
+import MaterialIcon from '../MaterialIcon';
+
 import {
 	Container
 } from './styles'
 
-interface ComponentProps {
+interface FilterSearchProps {
 };
 
-const Component: React.FC<ComponentProps> = ({ ... rest }) => {
+// TODO: Corrigir icone
+// TODO: Estilizar select
+
+const FilterSearch: React.FC<FilterSearchProps> = ({ ... rest }) => {
 	
 	return (
 		<Container { ... rest } > 
+			<MaterialIcon name="task" size={26} color="black" />
+
+			<select name="filter" id="filter">
+				<option value="01">Teste</option>
+				<option value="02">Teste</option>
+				<option value="03">Teste</option>
+				<option value="04">Teste</option>
+			</select>
+
 		</Container> 
 	);
 
 };
 
-export default Component;
+export default FilterSearch;
 
--- src/components/FilterSearch/styles.ts
+++ src/components/FilterSearch/styles.ts
@@ -1,3 +1,17 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+	box-shadow: 0px 4px 4px 0px #00000040;
+
+	border-radius: 6px;	
+
+	display: flex;
+	flex: 0.1;
+
+	padding: 5px 10px;
+
+	> select {
+		margin-left: 10px;
+		width: 100%;
+	}
+`;
