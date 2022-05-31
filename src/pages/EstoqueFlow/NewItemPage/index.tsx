9c909fc56722edd2471fc97c509c3d7def2066ad
exodojaffar <exodojaffar@gmail.com>
2022-05-30 21:54:14 -0400

    pagina form

--- src/pages/EstoqueFlow/NewItemPage/index.tsx
+++ src/pages/EstoqueFlow/NewItemPage/index.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
 
+import GenericInput from '../../../components/GenericInput';
+import GenericButton from '../../../components/GenericButton'
+
 import {
-	Container
+	Container,
+	InputImage,
+	FormStyled,
+	OutrosInputs,
+	InputDuplo,
+
+	ButtonContainer,
 } from './styles';
+
+import addImageItem from '../../../assets/addImageItem.png'
 
 interface NewItemPageProps {
 };
@@ -14,6 +25,45 @@ const NewItemPage: React.FC<NewItemPageProps> = () => {
 			titleHeader="Cadastro de mercadoria" 
 			subTitleHeader="Preencha os campos e cadastre um produto."
 		>
+
+			<FormStyled>
+				<InputImage>
+					<input  type="image" src={addImageItem} alt=""/>
+				</InputImage>
+
+				<OutrosInputs>
+					<GenericInput 
+						textLabel="Nome"
+						name="name"
+					/>
+
+					<InputDuplo>
+						<GenericInput 
+							textLabel="Preço"
+							name="price"
+							inputType="number"
+							placeholder="0,00"
+							textSide="R$"
+						/>
+						<GenericInput 
+							textLabel="Estoque"
+							name="quant"
+							inputType="number"
+							placeholder="0"
+							textSide="unid."
+							sideOnRight={false}
+						/>
+					</InputDuplo>
+					
+				</OutrosInputs>
+
+			</FormStyled> 
+
+			<ButtonContainer>
+				<GenericButton text="Criar novo item" borderRadius={30} iconName="task" iconSize={36}/>
+			</ButtonContainer>
+
+			
 		</Container>
 	);
 }
--- src/pages/EstoqueFlow/NewItemPage/styles.ts
+++ src/pages/EstoqueFlow/NewItemPage/styles.ts
@@ -5,3 +5,47 @@ import BasePage from '../../../components/BasePage'
 // COMPONENTES DE BASE
 export const Container = styled(BasePage)`
 `;
+
+export const InputImage = styled.div`
+	border: 1.25px dashed #333333;
+`;
+
+export const FormStyled = styled.form`
+	display: flex;
+
+	padding: 12px;
+
+	> img{
+		height: 100px;
+		width: 220px;	
+	}
+`;
+
+export const OutrosInputs = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+
+	flex: 1;
+
+	margin-left: 20px;
+`;
+
+export const InputDuplo = styled.div`
+	display: flex;
+	justify-content: space-between;
+
+	> * {
+		flex-basis: 1px;
+	}
+`;
+
+export const ButtonContainer = styled.div`
+	display: flex; 
+	flex:1;
+
+	justify-content: center;
+	align-items: center;
+
+	margin-top: 24px;
+`;
