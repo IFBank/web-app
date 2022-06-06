47c7519f091ee1231954a2a42e6e835ef7861e75
exodojaffar <exodojaffar@gmail.com>
2022-06-05 20:42:55 -0400

    fix icon

--- src/components/FilterSearch/index.tsx
+++ src/components/FilterSearch/index.tsx
@@ -16,13 +16,13 @@ const FilterSearch: React.FC<FilterSearchProps> = ({ ... rest }) => {
 	
 	return (
 		<Container { ... rest } > 
-			<MaterialIcon name="task" size={26} color="black" />
+			<MaterialIcon name="filter_list" size={26} color="#32CD32" />
 
 			<select name="filter" id="filter">
-				<option value="01">Teste</option>
-				<option value="02">Teste</option>
-				<option value="03">Teste</option>
-				<option value="04">Teste</option>
+				<option value="01">Mais recentes</option>
+				<option value="02">Mais antigos</option>
+				<option value="03">Maior valor </option>
+				<option value="04">Meno valor</option>
 			</select>
 
 		</Container> 
